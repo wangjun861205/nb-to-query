58145cf0ffe341a5eedb287e291290ea1f4ff28a
wangjun <444055828@qq.com>
2023-12-03 00:45:40 +0800

    v0.1

---
+++ src/lib.rs
@@ -0,0 +1,150 @@
+#![feature(iter_intersperse)]
+
+pub use nb_to_query_derive::ToQueryDerive;
+
+pub trait ToQuery {
+    fn to_query(&self, field_name: &str) -> Option<String>;
+}
+
+impl ToQuery for String {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for &str {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for bool {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for i8 {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for i16 {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for i32 {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for i64 {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for i128 {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for u8 {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for u16 {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for u32 {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for u64 {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for u128 {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for f32 {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl ToQuery for f64 {
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        Some(format!("{}={}", field_name, self))
+    }
+}
+
+impl<T> ToQuery for Vec<T>
+where
+    T: ToQuery,
+{
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        if self.is_empty() {
+            return None;
+        }
+        let pairs: String = self
+            .iter()
+            .flat_map(|v| v.to_query(field_name))
+            .intersperse("&".into())
+            .collect();
+        if pairs.is_empty() {
+            return None;
+        }
+        Some(pairs)
+    }
+}
+
+impl<T> ToQuery for Option<T>
+where
+    T: ToQuery,
+{
+    fn to_query(&self, field_name: &str) -> Option<String> {
+        self.as_ref().map(|v| v.to_query(field_name))?
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(ToQueryDerive)]
+    struct TestStruct {
+        a: String,
+        b: bool,
+        c: i32,
+        d: Vec<String>,
+    }
+    #[test]
+    fn test_to_query() {
+        let test_struct = TestStruct {
+            a: "a".to_string(),
+            b: true,
+            c: 1,
+            d: vec!["a".to_string(), "b".to_string()],
+        };
+        let query = test_struct.to_query("");
+        assert_eq!(query, Some("a=a&b=true&c=1&d=a&d=b".into()));
+    }
+}
